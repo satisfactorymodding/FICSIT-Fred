ae57410cbb3c3f38008f0b8beac9dcd2da9f01f7
FICSIT-Fred <feykook@gmail.com>
2020-06-20 21:19:44 +0200

    Vilsol's life is now massively easier
    Added config_example, removed config, bot now checks for config and creates config from the example if config doesn't exist

--- Scripts/CreateEmbed.py
+++ Scripts/CreateEmbed.py
@@ -3,7 +3,11 @@ import datetime
 import requests
 import json
 import Helper
+import os
+import shutil
 
+if not os.path.exists("config/config.json"):
+    shutil.copyfile("config/config_example.json", "config/config.json")
 with open("config/config.json", "r") as file:
     Config = json.load(file)
 
