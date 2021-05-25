bb2211d50788f5c81f48d46ad3df4f1c2df7064a
Astro Floof <does.not.kompute@gmail.com>
2021-05-25 09:49:06 -0400

    mediaonly.py formatting of media only removal message

--- bot/cogs/mediaonly.py
+++ bot/cogs/mediaonly.py
@@ -13,13 +13,12 @@ class MediaOnly(commands.Cog):
         if len(message.embeds) > 0 or len(message.attachments) > 0:
             return
         if config.MediaOnlyChannels.fetch(message.channel.id):
-            self.bot.send_DM( message.author,
-                "Hi " + message.author.name + ", the channel '" + self.bot.get_channel(message.channel.id).name
-                + "' you just tried to message in has been flagged as a 'Media Only' "
-                  "channel. This means you must attach a file in order to "
-                  "post there. Here is your "
-                  "message if you want to paste it again after the adequate changes : "
-                  "```\n" + message.content + "\n```")
+            self.bot.send_DM(message.author,
+                             f"Hi {message.author.name}, the channel you just tried to message in, "
+                             f"{self.bot.get_channel(message.channel.id).name} is a 'Media Only' channel. "
+                             f"This means you must attach a file in order to post there. "
+                             f"Here is your message if you want to paste it again after the adequate changes: "
+                             f"```\n{message.content}\n```")
             await message.delete()
             return True
         return False
