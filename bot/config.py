1446892318e80815a767d88cfb9df3cc2f1cfc3d
Astro Floof <does.not.kompute@gmail.com>
2021-05-28 11:51:51 -0400

    config.py oops closing parenthesis

--- bot/config.py
+++ bot/config.py
@@ -328,4 +328,4 @@ def convert_old_config():
                 for i in v:
                     ReservedCommands(name=i["command"])
             else:
-                raise Exception(f"found non supported config key: {k}"
+                raise Exception(f"found non supported config key: {k}")
