4194af068df0253b4b90af6f259b239bc51c2c21
AstroFloof <does.not.kompute@gmail.com>
2021-07-04 15:42:11 -0400

    repair mediaonly DM and optimize test priority

--- bot/cogs/mediaonly.py
+++ bot/cogs/mediaonly.py
@@ -8,17 +8,19 @@ class MediaOnly(commands.Cog):
         self.bot = bot
 
     async def process_message(self, message):
-        if t3_only(message):
-            return False
+
         if len(message.embeds) > 0 or len(message.attachments) > 0:
             return
+        if await t3_only(message, self.bot):
+            return False
         if config.MediaOnlyChannels.fetch(message.channel.id):
-            self.bot.send_DM(message.author,
-                             f"Hi {message.author.name}, the channel you just tried to message in, "
-                             f"{self.bot.get_channel(message.channel.id).name} is a 'Media Only' channel. "
-                             f"This means you must attach a file in order to post there. "
-                             f"Here is your message if you want to paste it again after the adequate changes: "
-                             f"```\n{message.content}\n```")
+            await self.bot.send_DM(message.author,
+                                   f"Hi {message.author.name}, the channel you just tried to message in, "
+                                   f"{self.bot.get_channel(message.channel.id).name} is a 'Media Only' channel. "
+                                   f"This means you must attach a file in order to post there. "
+                                   f"Here is your message if you want to paste it again after the adequate changes: "
+                                   f"```\n{message.content}\n```")
             await message.delete()
             return True
+        print("Skipped MediaOnly for some unfathomable reason")
         return False
