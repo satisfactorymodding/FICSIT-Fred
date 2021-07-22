dbed15c1fe2ca49a781f3cb4859e60e82f05edf5
Feyko <feykook@gmail.com>
2021-07-23 00:35:58 +0200

    Bump version

--- bot/bot.py
+++ bot/bot.py
@@ -36,7 +36,7 @@ class Bot(discord.ext.commands.Bot):
         self.setup_DB()
         self.command_prefix = config.Misc.fetch("prefix")
         self.setup_cogs()
-        self.version = "2.11.2"
+        self.version = "2.12.0"
 
         self.running = True
         self.loop = asyncio.get_event_loop()
