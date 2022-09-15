1dd2d69690949eb0f31564971b92018cfea31785
Feyko <feykook@gmail.com>
2022-09-15 21:43:55 +0200

    chore: proper exception debug log

--- fred/fred.py
+++ fred/fred.py
@@ -17,7 +17,7 @@ from .cogs import crashes, dialogflow, mediaonly, webhooklistener, welcome, leve
 from .libraries import createembed, common
 
 
-__version__ = "2.20.1"
+__version__ = "2.20.2"
 
 
 class Bot(commands.Bot):
