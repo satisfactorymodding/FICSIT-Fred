satisfactorymodding/FICSIT-Fred

commit 4e123d22f3804ebab2e762d91d69c5ec3629c3fc

fix: cleanup and minor fixes
- fixed an issue where discord file urls weren't resolving anymore
- expanded functionality of common.permission_check
- handled error due to hybrid running not having permissions to open port 80

fred/cogs/crashes.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import asyncio
 import io
 import json
@@ -6,11 +8,14 @@ import re
 import traceback
 import zipfile
 from concurrent.futures import ThreadPoolExecutor
+from os.path import split
 from time import strptime
-from typing import AsyncIterator
+from typing import AsyncIterator, IO
+from urllib.parse import urlparse
 
 import nextcord
 from PIL import Image, ImageEnhance, UnidentifiedImageError
+from nextcord import Message
 from pytesseract import image_to_string, TesseractError
 
 from .. import config
@@ -50,7 +55,13 @@ class Crashes(FredCog):
         vanilla_info_search_area = filter(lambda l: re.match("^LogInit", l), lines)
 
         info = {}
-        patterns = self.vanilla_info_patterns[:]
+        patterns = self.vanilla_info_patterns[:]  # shallow copy
+        """
+        This godforsaken loop sequentially finds information, 
+        dropping patterns to look for as they are found (until it runs out of patterns).
+        The patterns have named regex captures which the rest of the code knows the names of.
+        - Borketh
+        """
         for line in vanilla_info_search_area:
             if not patterns:
                 break
@@ -185,7 +196,7 @@ class Crashes(FredCog):
             if latest_compatible_loader > strptime(mod["last_version_date"], "%Y-%m-%dT%H:%M:%S.%fZ")
         ]
 
-    async def process_file(self, file, extension) -> list[dict]():
+    async def process_file(self, file: IO, extension: str) -> list[dict]():
         self.logger.info(f"Processing {extension} file")
         match extension:
             case "":
@@ -326,7 +337,7 @@ class Crashes(FredCog):
     async def process_text(self, text: str) -> list[dict]:
         return [msg async for msg in self.mass_regex(text)]
 
-    async def process_message(self, message) -> bool:
+    async def process_message(self, message: Message) -> bool:
         self.bot.logger.info("Processing crashes")
         responses: list[dict] = []
         # attachments
@@ -343,9 +354,10 @@ class Crashes(FredCog):
                 self.logger.info("Couldn't acquire file from discord API")
                 try:
                     self.logger.info("Attempting to acquire linked file manually")
-                    url = cdn_link.group(1)
-                    name = url.split("/")[-1]
-                    async with self.bot.web_session.get(url) as response:
+                    att_url = cdn_link.group(1)
+                    url_path = urlparse(att_url).path
+                    _, name = split(url_path)
+                    async with self.bot.web_session.get(att_url) as response:
                         assert response.status == 200, f"the web request failed with status {response.status}"
                         file = io.BytesIO(await response.read())
                 except (AttributeError, AssertionError) as e:
@@ -381,10 +393,10 @@ class Crashes(FredCog):
             await self.bot.reply_to_msg(message, embed=createembed.crashes(responses))
         else:
             for response in responses:
-                attachment = response.get("attachment")
-                if attachment is not None:
-                    async with self.bot.web_session.get(attachment) as resp:
+                att_url: str = response.get("attachment")
+                if att_url is not None:
+                    async with self.bot.web_session.get(att_url) as resp:
                         buff = io.BytesIO(await resp.read())
-                        attachment = nextcord.File(filename=attachment.split("/")[-1], fp=buff)
+                        attachment = nextcord.File(filename=att_url.split("/")[-1], fp=buff)
                 await self.bot.reply_to_msg(message, response["value"], file=attachment, propagate_reply=False)
         return len(responses) > 0

fred/cogs/dialogflow.py
@@ -1,7 +1,7 @@
 import asyncio
 import json
-import logging
 import uuid
+from os import getenv
 
 import nextcord
 from google.cloud import dialogflow
@@ -10,14 +10,17 @@ from google.oauth2 import service_account
 from .. import config
 from ..libraries import common
 
-
-if os.environ.get("DIALOGFLOW_AUTH"):
-    DIALOGFLOW_AUTH = json.loads(os.environ.get("DIALOGFLOW_AUTH"))
+if env_auth_str := getenv("DIALOGFLOW_AUTH"):
+    DIALOGFLOW_AUTH = json.loads(env_auth_str)
     session_client = dialogflow.SessionsClient(
         credentials=service_account.Credentials.from_service_account_info(DIALOGFLOW_AUTH)
     )
     DIALOGFLOW_PROJECT_ID = DIALOGFLOW_AUTH["project_id"]
     SESSION_LIFETIME = 10 * 60  # 10 minutes to avoid repeated false positives
+else:
+    DIALOGFLOW_AUTH = None
+    session_client = None
+    DIALOGFLOW_PROJECT_ID = None
 
 
 class DialogFlow(common.FredCog):
@@ -36,7 +39,7 @@ class DialogFlow(common.FredCog):
             # We're in a DM channel
             self.logger.info("Ignoring a message because it is in a DM channel", extra=common.message_info(message))
             return
-            self.logger.info("Ignoring a message because NLP is disabled", extra=common.message_info(message))
+        if not config.Misc.fetch("dialogflow_state") or DIALOGFLOW_AUTH is None:
             self.logger.info(
                 "Ignoring a message because NLP is disabled or not configured", extra=common.message_info(message)
             )

fred/cogs/levelling.py
@@ -4,7 +4,7 @@ import logging
 import math
 from datetime import *
 
-import nextcord.ext.commands as commands
+from nextcord import DMChannel, Message, Guild
 from nextcord.ext.commands import MemberNotFound
 
 from .. import config
@@ -14,11 +14,9 @@ logger = common.new_logger(__name__)
 
 
 class UserProfile:
-    def __init__(self, user_id, guild, bot):
+    def __init__(self, user_id: int, guild: Guild):
         self.guild = guild
-        self.bot = bot
         self.user_id = user_id
-        self.logger = logging.Logger("USERPROFILE")
 
         self.member = guild.get_member(user_id)
         if self.member is None:
@@ -32,8 +30,13 @@ class UserProfile:
         else:
             self.DB_user = config.Users(user_id=user_id)
 
-        self.rank = self.DB_user.rank
-        self.xp_count = self.DB_user.xp_count
+    @property
+    def rank(self):
+        return self.DB_user.rank
+
+    @property
+    def xp_count(self):
+        return self.DB_user.xp_count
 
     async def validate_role(self):
         if not self.member:
@@ -87,12 +90,12 @@ class UserProfile:
             self.DB_user.rank = expected_level
             if self.DB_user.accepts_dms:
                 if expected_level > self.rank:
-                    await self.bot.send_DM(
+                    await Levelling.bot.send_DM(
                         self.member,
                         f"You went up from level {self.rank} to level {expected_level}! " f"Congratulations!",
                     )
                 else:
-                    await self.bot.send_DM(
+                    await Levelling.bot.send_DM(
                         self.member,
                         f"You went down from level {self.rank} to level {expected_level}... " f"Sorry about that",
                     )
@@ -112,7 +115,6 @@ class UserProfile:
         logpayload["xp_gain"] = xp
         logger.info("Giving someone xp", logpayload)
         self.DB_user.xp_count += xp
-        self.xp_count += xp
         await self.validate_level()
         return True
 
@@ -124,7 +126,6 @@ class UserProfile:
         logpayload["xp_loss"] = xp
         logger.info("Taking xp from someone", logpayload)
         self.DB_user.xp_count -= xp
-        self.xp_count -= xp
         await self.validate_level()
         return True
 
@@ -133,7 +134,6 @@ class UserProfile:
         logpayload["new_xp"] = xp
         logger.info("Setting someone's xp", logpayload)
         self.DB_user.xp_count = xp
-        self.xp_count = xp
         await self.validate_level()
         return True
 
@@ -151,8 +151,8 @@ class Levelling(common.FredCog):
     #     if before.roles != after.roles:
     #         config.XpRoles
 
-    @commands.Cog.listener()
     @common.FredCog.listener()
+    async def on_message(self, message: Message):
         self.logger.info("Levelling: Processing message", extra=common.message_info(message))
         if (
             message.author.bot
@@ -162,7 +162,7 @@ class Levelling(common.FredCog):
         ):
             return
 
-        profile = UserProfile(message.author.id, message.guild, self.bot)
+        profile = UserProfile(message.author.id, message.guild)
         profile.DB_user.message_count += 1
         if profile.user_id in self.xp_timers:
             if datetime.now() >= self.xp_timers[profile.user_id]:

fred/cogs/webhooklistener.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import asyncio
 import json
 import socket
@@ -5,8 +7,13 @@ import sys
 import threading
 import traceback
 from http.server import BaseHTTPRequestHandler, HTTPServer
+from os import getenv
+from typing import TYPE_CHECKING
 
 from fred.libraries import common
+
+if TYPE_CHECKING:
+    from fred.fred import Bot
 
 logger = common.new_logger(__name__)
 
@@ -29,12 +36,11 @@ class Githook(common.FredCog):
 
         # Run GitHub webhook handling server
         try:
-            botargs = [bot, bot]
-            daemon = threading.Thread(target=runServer, args=botargs)
+            daemon = threading.Thread(target=runServer, args=[self.bot, self.bot])
             daemon.daemon = True
             daemon.start()
-        except Exception:
-            type, value, tb = sys.exc_info()
+        except Exception:  # noqa
+            exc_type, value, tb = sys.exc_info()
             tbs = "".join(traceback.format_tb(tb))
             self.logger.error(f"Failed to run the webserver:\n{tbs}")
 
@@ -47,7 +53,7 @@ CONTENT_LEN = "content-length"
 EVENT_TYPE = "x-github-event"
 
 
-def MakeGithookHandler(bot):
+def MakeGithookHandler(bot: Bot):
     class MyGithookHandler(BaseHTTPRequestHandler):
         def respond(self, code: int, message: str | None = None):
             self.send_response(code, message)

fred/config.py
@@ -1,12 +1,13 @@
 from __future__ import annotations
 
-from numbers import Number
-from typing import Optional, Any
+import json
 import os
 import pathlib
+from numbers import Number
+from typing import Optional, Any
 
 import nextcord
-from sqlobject import *
+from sqlobject import SQLObject, IntCol, BoolCol, JSONCol, BigIntCol, StringCol, FloatCol, sqlhub
 
 
 class PermissionRoles(SQLObject):

fred/fred.py
@@ -1,9 +1,8 @@
 import asyncio
-import logging
-import os
 import sys
 import time
 import traceback
+from os import getenv
 
 import aiohttp
 import nextcord
@@ -48,7 +47,7 @@ class Bot(commands.Bot):
 
     async def start(self, *args, **kwargs):
         async with aiohttp.ClientSession() as session:
-            self.web_session = session
+            self.web_session = session  # noqa
             return await super().start(*args, **kwargs)
 
     @staticmethod
@@ -67,11 +66,11 @@ class Bot(commands.Bot):
 
     def setup_DB(self):
         self.logger.info("Connecting to the database")
-        user = os.environ.get("FRED_SQL_USER")
-        password = os.environ.get("FRED_SQL_PASSWORD")
-        host = os.environ.get("FRED_SQL_HOST")
-        port = os.environ.get("FRED_SQL_PORT")
-        dbname = os.environ.get("FRED_SQL_DB")
+        user = getenv("FRED_SQL_USER")
+        password = getenv("FRED_SQL_PASSWORD")
+        host = getenv("FRED_SQL_HOST")
+        port = getenv("FRED_SQL_PORT")
+        dbname = getenv("FRED_SQL_DB")
         uri = f"postgresql://{user}:{password}@{host}:{port}/{dbname}"
         attempt = 1
         while attempt < 6:
@@ -101,8 +100,20 @@ class Bot(commands.Bot):
 
         self.logger.info("Successfully set up cogs")
 
+    @property
+    def MediaOnly(self) -> mediaonly.MediaOnly:
+        return self.get_cog("MediaOnly")  # noqa
+
+    @property
+    def Crashes(self) -> crashes.Crashes:
+        return self.get_cog("Crashes")  # noqa
+
+    @property
+    def DialogFlow(self) -> dialogflow.DialogFlow:
+        return self.get_cog("DialogFlow")  # noqa
+
     async def on_error(self, event, *args, **kwargs):
-        type, value, tb = sys.exc_info()
+        exc_type, value, tb = sys.exc_info()
         if event == "on_message":
             channel: nextcord.TextChannel | nextcord.DMChannel = args[0].channel
             if isinstance(channel, nextcord.DMChannel):
@@ -113,7 +124,7 @@ class Bot(commands.Bot):
             channel_str = ""
 
         fred_str = f"Fred v{self.version}"
-        error_meta = f"{type.__name__} exception handled in `{event}` {channel_str}"
+        error_meta = f"{exc_type.__name__} exception handled in `{event}` {channel_str}"
         full_error = f"\n{value}\n\n{''.join(traceback.format_tb(tb))}"
         self.logger.error(f"{fred_str}\n{error_meta}\n{full_error}")
 
@@ -135,7 +146,7 @@ class Bot(commands.Bot):
 
     async def send_DM(
         self,
-        user: nextcord.User,
+        user: nextcord.User | nextcord.Member,
         content: str = None,
         embed: nextcord.Embed = None,
         user_meta: config.Users = None,
@@ -168,7 +179,7 @@ class Bot(commands.Bot):
                 content = None
             await user.dm_channel.send(content=content, embed=embed, **kwargs)
             return True
-        except Exception:
+        except Exception:  # noqa
             self.logger.error(f"DMs: Failed to DM, reason: \n{traceback.format_exc()}")
         return False
 

fred/fred_commands/__init__.py
@@ -1,21 +1,20 @@
 from __future__ import annotations
 
-import logging
-
 import asyncio
 import inspect
 import io
+import logging
 import re
 
 import nextcord
-from nextcord.ext.commands.view import StringView
 from algoliasearch.search_client import SearchClient
+from nextcord.ext.commands.view import StringView
 
 from ._baseclass import BaseCmds, common, config, commands
 from .bot_meta import BotCmds
 from .channels import ChannelCmds
-from .dbcommands import CommandCmds
 from .crashes import CrashCmds
+from .dbcommands import CommandCmds
 from .dialogflow import DialogflowCmds
 from .experience import EXPCmds
 from .help import HelpCmds, FredHelpEmbed

fred/libraries/common.py
@@ -2,10 +2,11 @@ from __future__ import annotations
 
 import logging
 import re
-from functools import lru_cache
+from functools import lru_cache, singledispatch
 from typing import TYPE_CHECKING, Optional
 
 from nextcord import User, Message, Member
+from nextcord.ext import commands
 from nextcord.ext.commands import Context
 
 from .. import config
@@ -40,34 +41,45 @@ def is_bot_author(user_id: int) -> bool:
 
 async def l4_only(ctx: Context) -> bool:
     logger.info("Checking if someone is a T3", extra=user_info(ctx.author))
-    return is_bot_author(ctx.author.id) or permission_check(ctx, 4)
+    return is_bot_author(ctx.author.id) or permission_check(ctx, level=4)
 
 
 async def mod_only(ctx: Context) -> bool:
     logger.info("Checking if someone is a Moderator", extra=user_info(ctx.author))
-    return is_bot_author(ctx.author.id) or permission_check(ctx, 6)
+    return is_bot_author(ctx.author.id) or permission_check(ctx, level=6)
 
 
-def permission_check(ctx: Context, level: int) -> bool:
-    logpayload = user_info(ctx.author)
-    logpayload["level"] = level
-
-    logger.info("Checking permissions for someone", extra=logpayload)
-    perms = config.PermissionRoles.fetch_by_lvl(level)
-
-    main_guild_id = int(config.Misc.fetch("main_guild_id"))
+@singledispatch
+def permission_check(ctx: Context, *, level: int) -> bool:
+    main_guild_id = config.Misc.fetch("main_guild_id")
     main_guild = ctx.bot.get_guild(main_guild_id)
+
     if main_guild is None:
         raise LookupError(f"Unable to retrieve the guild {main_guild_id}. Is this the guild you meant?")
 
     if (main_guild_member := main_guild.get_member(ctx.author.id)) is None:
-        logger.warning("Checked permissions for someone but they weren't in the main guild", extra=logpayload)
         logger.warning(
             "Checked permissions for someone but they weren't in the main guild", extra=user_info(ctx.author)
         )
         return False
 
-    user_roles = [role.id for role in main_guild_member.roles]
+    return permission_check(main_guild_member, level=level)
+
+
+@permission_check.register
+def permission_check(member: Member, *, level: int) -> bool:
+    """Checks permissions for a member assuming they are in the main guild."""
+    logpayload = user_info(member)
+    logpayload["level"] = level
+
+    if member.guild.id != config.Misc.fetch("main_guild_id"):
+        logger.warning("Checked permissions for a member of the wrong guild", extra=logpayload)
+        return False
+
+    logger.info("Checking permissions for someone", extra=logpayload)
+    perms = config.PermissionRoles.fetch_by_lvl(level)
+
+    user_roles = [role.id for role in member.roles]
     if (
         # it shouldn't be possible to request a level above the defined levels but check anyway
         role := next(
@@ -82,16 +94,18 @@ def permission_check(ctx: Context, level: int) -> bool:
     return False
 
 
-@lru_cache(5)
-def user_info(user: User | Member) -> dict:
+@lru_cache(15)
+def user_info(user: Optional[User | Member]) -> dict:
+    if user is None:
+        return {}
     return {"user_full_name": user.global_name, "user_id": user.id}
 
 
-@lru_cache(5)
-def message_info(message: Message) -> dict:
+@lru_cache(15)
+def message_info(message: Optional[Message]) -> dict:
     if message is None:
         return {}
-    return {"message_id": message.id, "channel_id": message.channel.id, "user_id": message.author.id}
+    return {"message_id": message.id, "channel_id": message.channel.id, **user_info(message.author)}
 
 
 def reduce_str(string: str) -> str:

commit b1942dee0ee03566c48811d3b86cdf3749a0f79d

perf(crashes): better debug zip responsiveness
eased off regex timeout
used nonblocking regex lib
better async handling

also fixes bug introduced in 4e123d22f3804ebab2e762d91d69c5ec3629c3fc where the attachment to a response could be undefined instead of a safe None

fred/cogs/crashes.py
@@ -4,7 +4,6 @@ import asyncio
 import io
 import json
 import logging
-import re
 import traceback
 import zipfile
 from concurrent.futures import ThreadPoolExecutor
@@ -14,6 +13,7 @@ from typing import AsyncIterator, IO
 from urllib.parse import urlparse
 
 import nextcord
+import regex
 from PIL import Image, ImageEnhance, UnidentifiedImageError
 from nextcord import Message
 from pytesseract import image_to_string, TesseractError
@@ -22,12 +22,12 @@ from .. import config
 from ..libraries import createembed
 from ..libraries.common import FredCog
 
-REGEX_LIMIT: float = 2
+REGEX_LIMIT: float = 6.9
 
 
-async def regex_with_timeout(*args, **kwargs):
+def regex_with_timeout(*args, **kwargs):
     try:
-        return await asyncio.wait_for(asyncio.to_thread(re.search, *args, **kwargs), REGEX_LIMIT)
+        return asyncio.wait_for(asyncio.to_thread(regex.search, *args, **kwargs), REGEX_LIMIT)
     except asyncio.TimeoutError:
         raise TimeoutError(
             f"A regex timed out after {REGEX_LIMIT} seconds! \n"
@@ -39,10 +39,10 @@ async def regex_with_timeout(*args, **kwargs):
 
 class Crashes(FredCog):
     vanilla_info_patterns = [
-        re.compile(r"Net CL: (?P<game_version>\d+)"),
-        re.compile(r"Command Line: (?P<cli>.*)"),
-        re.compile(r"Base Directory: (?P<path>.+)"),
-        re.compile(r"Launcher ID: (?P<launcher>\w+)"),
+        regex.compile(r"Net CL: (?P<game_version>\d+)"),
+        regex.compile(r"Command Line: (?P<cli>.*)"),
+        regex.compile(r"Base Directory: (?P<path>.+)"),
+        regex.compile(r"Launcher ID: (?P<launcher>\w+)"),
     ]
 
     @staticmethod
@@ -52,7 +52,7 @@ class Crashes(FredCog):
     async def parse_factory_game_log(self, text: str) -> dict[str, str | int]:
         self.logger.info("Extracting game info")
         lines = text.splitlines()
-        vanilla_info_search_area = filter(lambda l: re.match("^LogInit", l), lines)
+        vanilla_info_search_area = filter(lambda l: regex.match("^LogInit", l), lines)
 
         info = {}
         patterns = self.vanilla_info_patterns[:]  # shallow copy
@@ -71,7 +71,7 @@ class Crashes(FredCog):
         else:
             self.logger.info("Didn't find all four pieces of information normally found in a log")
 
-        mod_loader_logs = filter(lambda l: re.match("LogSatisfactoryModLoader", l), lines)
+        mod_loader_logs = filter(lambda l: regex.match("LogSatisfactoryModLoader", l), lines)
         for line in mod_loader_logs:
             if match := await regex_with_timeout(r"(?<=v\.)(?P<sml>[\d.]+)", line):
                 info |= match.groupdict()
@@ -309,8 +309,15 @@ class Crashes(FredCog):
                     return []
 
     async def mass_regex(self, text: str) -> AsyncIterator[dict]:
-        for crash in config.Crashes.fetch_all():
-            if match := await regex_with_timeout(crash["crash"], text, flags=re.IGNORECASE):
+
+        async def silly_async_gen():
+            nonlocal text
+            all_crashes = config.Crashes.fetch_all()
+            for crash_ in all_crashes:
+                yield crash_, regex_with_timeout(crash_["crash"], text, flags=regex.IGNORECASE)
+
+        async for crash, queued_regex in silly_async_gen():
+            if match := await queued_regex:
                 if str(crash["response"]).startswith(self.bot.command_prefix):
                     if command := config.Commands.fetch(crash["response"].strip(self.bot.command_prefix)):
                         command_response = command["content"]
@@ -323,7 +330,7 @@ class Crashes(FredCog):
                             inline=True,
                         )
                 else:
-                    response = re.sub(r"{(\d+)}", lambda m: match.group(int(m.group(1))), str(crash["response"]))
+                    response = regex.sub(r"{(\d+)}", lambda m: match.group(int(m.group(1))), str(crash["response"]))
                     yield dict(name=crash["name"], value=response, inline=True)
 
     async def complex_responses(self, log_details: dict):
@@ -341,7 +348,7 @@ class Crashes(FredCog):
         self.bot.logger.info("Processing crashes")
         responses: list[dict] = []
         # attachments
-        cdn_link = re.search(r"(https://cdn.discordapp.com/attachments/\S+)", message.content)
+        cdn_link = regex.search(r"(https://cdn.discordapp.com/attachments/\S+)", message.content)
         if cdn_link or message.attachments:
             self.logger.info("Found file in message")
 
@@ -377,8 +384,8 @@ class Crashes(FredCog):
             file.close()
 
         # Pastebin links
-        elif match := re.search(r"(https://pastebin.com/\S+)", message.content):
-            url = re.sub(r"(?<=bin.com)/", "/raw/", match.group(1))
+        elif match := regex.search(r"(https://pastebin.com/\S+)", message.content):
+            url = regex.sub(r"(?<=bin.com)/", "/raw/", match.group(1))
             async with self.bot.web_session.get(url) as response:
                 text: str = await response.text()
 
@@ -398,5 +405,7 @@ class Crashes(FredCog):
                     async with self.bot.web_session.get(att_url) as resp:
                         buff = io.BytesIO(await resp.read())
                         attachment = nextcord.File(filename=att_url.split("/")[-1], fp=buff)
+                else:
+                    attachment = None
                 await self.bot.reply_to_msg(message, response["value"], file=attachment, propagate_reply=False)
         return len(responses) > 0