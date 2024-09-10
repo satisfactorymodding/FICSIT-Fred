1f54fd1d2debaa6215d6c445c4f6e7f6f54228ef
borketh <does.not.kompute@gmail.com>
2024-09-10 14:35:20 -0400

    chore: version bump (2.22.0)

--- fred/fred.py
+++ fred/fred.py
@@ -18,7 +18,7 @@ from .cogs import crashes, dialogflow, mediaonly, webhooklistener, welcome, leve
 from .fred_commands import Commands, FredHelpEmbed
 from .libraries import createembed, common
 
-__version__ = "2.21.1"
+__version__ = "2.22.0"
 
 
 class Bot(commands.Bot):
