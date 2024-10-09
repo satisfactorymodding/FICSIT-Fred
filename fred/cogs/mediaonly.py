33945561bc8e9a17e1da401cac688f50f025116f
borketh <does.not.kompute@gmail.com>
2024-10-08 22:00:25 -0400

    feat: mediaonly forum channels

--- fred/cogs/mediaonly.py
+++ fred/cogs/mediaonly.py
@@ -1,4 +1,4 @@
-from nextcord import Message
+from nextcord import Message, Thread, ForumChannel
 
 from .. import config
 from ..libraries import common
@@ -9,7 +9,22 @@ class MediaOnly(FredCog):
 
     async def process_message(self, message: Message) -> bool:
         """Returns whether the message was removed."""
-        self.logger.info("Processing a message", extra=common.message_info(message))
+        if isinstance(message.channel, Thread):
+            if (
+                isinstance(message.channel.parent, ForumChannel)
+                and message.id == message.channel.id  # we only care if it's the initial post
+                and config.MediaOnlyChannels.check(message.channel.parent_id)  # last because it's a DB call
+            ):
+                return await self._process_forum_message(message)
+            else:
+                return False
+        elif config.MediaOnlyChannels.check(message.channel.id):
+            return await self._process_regular_message(message)
+
+        return False
+
+    async def _process_regular_message(self, message: Message) -> bool:
+        self.logger.info("Checking a message", extra=common.message_info(message))
 
         if len(message.embeds) > 0 or len(message.attachments) > 0:
             self.logger.info("Message contains media", extra=common.message_info(message))
@@ -21,17 +36,41 @@ class MediaOnly(FredCog):
             self.logger.info("Message doesn't contain media but the author is a T3", extra=common.message_info(message))
             return False
 
-        if config.MediaOnlyChannels.check(message.channel.id):
-            self.logger.info("Removing a message", extra=common.message_info(message))
-            await message.delete()
-            await self.bot.send_DM(
-                message.author,
-                f"Hi {message.author.name}, the channel you just tried to message in, "
-                f"{self.bot.get_channel(message.channel.id).name} is a 'Media Only' channel. "
-                f"This means you must attach a file in order to post there. "
-                f"Here is your message if you want to paste it again after the adequate changes: "
-                f"```\n{message.content}\n```",
-            )
-            return True
+        self.logger.info("Removing a message", extra=common.message_info(message))
+        await message.delete()
+        await self.bot.send_DM(
+            message.author,
+            f"Hi {message.author.mention}, "
+            f"{message.channel.mention} is a 'media-only' channel. "
+            f"This means posts there must have files or embedded links. "
+            f"Here is your message if you want to paste it again after the adequate changes: "
+            f"```\n{message.content}\n```",
+        )
+        return True
 
-        return False
+    async def _process_forum_message(self, message: Message) -> bool:
+
+        self.logger.info("Checking a thread", extra=common.message_info(message))
+
+        if len(message.embeds) > 0 or len(message.attachments) > 0:
+            self.logger.info("Message contains media", extra=common.message_info(message))
+            return False
+
+        ctx = await self.bot.get_context(message)
+
+        if await common.l4_only(ctx):
+            self.logger.info("Message doesn't contain media but the author is a T3", extra=common.message_info(message))
+            return False
+
+        self.logger.info("Removing a thread", extra=common.message_info(message))
+        await message.channel.delete()
+        await self.bot.send_DM(
+            message.author,
+            f"Hi {message.author.mention}, "
+            f"{message.channel.parent.mention} is a 'media-only' forum. "
+            f"This means posts there must have files or embedded links. "
+            f"Here is your message if you want to paste it again after the adequate changes: "
+            f"\n\n**Title: **`{message.channel.name}`\n"
+            f"```\n{message.content}\n```",
+        )
+        return True
--- fred/fred_commands/channels.py
+++ fred/fred_commands/channels.py
@@ -1,4 +1,5 @@
-from nextcord import TextChannel
+from nextcord import TextChannel, ForumChannel
+from nextcord.abc import GuildChannel
 
 from ._baseclass import BaseCmds, commands, config, common
 
@@ -6,35 +7,36 @@ from ._baseclass import BaseCmds, commands, config, common
 class ChannelCmds(BaseCmds):
 
     @BaseCmds.add.command(name="mediaonly")
-    async def add_mediaonly(self, ctx: commands.Context, channel: commands.TextChannelConverter):
+    async def add_mediaonly(self, ctx: commands.Context, channel: commands.GuildChannelConverter):
         """Usage: `add mediaonly (channel)`
         Purpose: Adds channel to the list of channels that are managed to be media-only
         Notes: Limited to permission level 4 and above"""
-        channel: TextChannel
+        channel: GuildChannel
+
+        if not can_enforce_mediaonly(channel):
+            await self.bot.reply_to_msg(ctx.message, f"I don't know how to enforce mediaonly in {channel.mention}.")
+            return
+
         if config.MediaOnlyChannels.check(channel.id):
-            await self.bot.reply_to_msg(ctx.message, "This channel is already a media only channel")
+            await self.bot.reply_to_msg(ctx.message, f"{channel.mention} is already a media only channel")
             return
 
         config.MediaOnlyChannels(channel_id=channel.id)
-        await self.bot.reply_to_msg(
-            ctx.message, f"Media Only channel {self.bot.get_channel(channel.id).mention} added!"
-        )
+        await self.bot.reply_to_msg(ctx.message, f"Media-only channel {channel.mention} added!")
 
     @BaseCmds.remove.command(name="mediaonly")
-    async def remove_mediaonly(self, ctx: commands.Context, channel: commands.TextChannelConverter):
+    async def remove_mediaonly(self, ctx: commands.Context, channel: commands.GuildChannelConverter):
         """Usage: `add mediaonly (channel)`
         Purpose: Removes channel from the list of channels that are managed to be media-only
         Notes: Limited to permission level 4 and above"""
-        channel: TextChannel
+        channel: GuildChannel
 
         if not config.MediaOnlyChannels.check(channel.id):
-            await self.bot.reply_to_msg(ctx.message, "Media Only Channel could not be found!")
+            await self.bot.reply_to_msg(ctx.message, f"{channel.mention} is not marked as a media-only channel!")
             return
 
         config.MediaOnlyChannels.deleteBy(channel_id=channel.id)
-        await self.bot.reply_to_msg(
-            ctx.message, f"Media Only channel {self.bot.get_channel(channel.id).mention} removed!"
-        )
+        await self.bot.reply_to_msg(ctx.message, f"{channel.mention} is no longer a media-only channel.")
 
     @BaseCmds.add.command(name="dialogflowChannel")
     async def add_dialogflow_channel(self, ctx: commands.Context, channel: commands.TextChannelConverter):
@@ -91,3 +93,7 @@ class ChannelCmds(BaseCmds):
             self.bot.error_channel = error_channel_id
             config.Misc.create_or_change("error_channel", error_channel_id)
             await self.bot.reply_to_msg(ctx.message, f"The error channel has been changed to {chan.mention}.")
+
+
+def can_enforce_mediaonly(channel: GuildChannel) -> bool:
+    return isinstance(channel, (TextChannel, ForumChannel))
