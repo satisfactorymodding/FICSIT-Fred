ef77a63e006daecdb0aeda981f84779d52568e59
rhit-daughewd <148395658+rhit-daughewd@users.noreply.github.com>
2026-01-12 20:26:21 -0500

    Formatting

--- fred/libraries/regex_util.py
+++ fred/libraries/regex_util.py
@@ -6,6 +6,7 @@ REGEX_LIMIT: float = 6.9
 
 re2.set_fallback_module(regex_fallback)
 
+
 def pattern_uses_lookaround(pattern: str) -> bool:
     return bool(regex_fallback.search(r"\(\?=|\(\?!|\(\?<=|\(\?<!", pattern))
 
@@ -23,49 +24,16 @@ async def re2_search_with_timeout(pattern: str, text: str, flags=0):
 
 
 async def safe_search(pattern: str, text: str, flags=0):
-
     return await re2_search_with_timeout(pattern, text, flags=flags)
-
-    # if pattern_uses_lookaround(pattern):
-    #     return await asyncio.to_thread(regex_fallback.search, pattern, text, flags=flags)
-    # try:
-    #     return await re2_search_with_timeout(pattern, text, flags=flags)
-    # except re2.RegexError:
-    #     # fallback to the full-featured regex module
-    #     return await asyncio.to_thread(regex_fallback.search, pattern, text, flags=flags)
 
 
 def safe_search_sync(pattern: str, text: str, flags=0):
-
     return re2.search(pattern, text, flags=flags)
-
-    # if pattern_uses_lookaround(pattern):
-    #     return regex_fallback.search(pattern, text, flags=flags)
-    # try:
-    #     return re2.search(pattern, text, flags=flags)
-    # except re2.RegexError:
-    #     return regex_fallback.search(pattern, text, flags=flags)
 
 
 def safe_sub(pattern: str, repl, string: str, flags=0):
-
     return re2.sub(pattern, repl, string, flags=flags)
-
-    # if pattern_uses_lookaround(pattern):
-    #     return regex_fallback.sub(pattern, repl, string, flags=flags)
-    # try:
-    #     return re2.sub(pattern, repl, string, flags=flags)
-    # except re2.RegexError:
-    #     return regex_fallback.sub(pattern, repl, string, flags=flags)
 
 
 def safe_findall(pattern: str, string: str, flags=0):
-
     return re2.findall(pattern, string, flags=flags)
-
-    # if pattern_uses_lookaround(pattern):
-    #     return regex_fallback.findall(pattern, string, flags=flags)
-    # try:
-    #     return re2.findall(pattern, string, flags=flags)
-    # except re2.RegexError:
-    #     return regex_fallback.findall(pattern, string, flags=flags)
